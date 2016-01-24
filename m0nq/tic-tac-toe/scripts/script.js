47370e674ca1f3e395d3ad958e75620cafd4f4cb
Monq <monq.wellington@gmail.com>
2016-01-23 16:00:49 -0800

    create basic tic-tac-toe html and css stylingfor the div elements.

---
+++ m0nq/tic-tac-toe/scripts/script.js
@@ -0,0 +1,4 @@
+// create a game/global counter of clicks.
+// when an element is clicked and the counter is odd is player one's turn (x's)
+// when an element is clicked and the counter is even is player two's turn (o's)
+// reset the counter when the 'reset' button is clicked, & when the game ends.
