c3bdf303b882030338adabcc9ed7d9ab4a18112f
m0nq <monq.wellington@gmail.com>
2016-01-25 00:37:46 -0800

    finish the debugging process.

--- m0nq/tic-tac-toe/scripts/script.js
+++ m0nq/tic-tac-toe/scripts/script.js
@@ -1,15 +1,28 @@
-// create an array of arrays with all 8 winning node elements by their id names.
-// double loop through each to see if they have matching textConent.
-  // if true, then last player clicked wins
-
-
-
-var main = function main() {
+function main() {
   
   // create a game counter of clicks.
   var counter = 0;
+  // an array of arrays with string id combinations of winning situations.
+  var wins = [
+    ["#leftTop", "#centerTop", "#rightTop"],
+    ["#leftMiddle", "#centerMiddle", "#rightMiddle"],
+    ["#leftBot", "#centerBot", "#rightBot"],
+    ["#leftTop", "#leftMiddle", "#leftBot"],
+    ["#centerTop", "#centerMiddle", "#centerBot"],
+    ["#rightTop", "#rightMiddle", "#rightBot"],
+    ["#leftTop", "#centerMiddle", "#rightBot"],
+    ["#rightTop", "#centerMiddle", "#leftBot"]
+  ];
 
+  // html objects
+  // Banner to indicate who's turn it is.
+  var banner = document.querySelector("#turnBanner");
+  banner.textContent = "It's X's turn!";
+  // collection of the whole board
   var grid = document.querySelectorAll(".grid");
+  // html button object.
+  var button = document.querySelector("button");
+  button.addEventListener("click", reset);
 
   // register event listeners on each of the inner divs.
   registrar(grid);
@@ -25,23 +38,47 @@ var main = function main() {
   // Event -> Void
   // check the various conditions under which the game may be over before adding the players mark.
   function checkPoint(event) {
-    // if counter reaches 6 without a winner, then it's a tie.
-    if (counter == 6) {
-      catsGame();
-      // else, if 'X' has a match, 'X' wins
-    } else if (match(event.target)) {
+
+    clicked(event);
+
+    // check to see if there is a matching set
+    if (match(event.target)) {
       winner(event.target);
-    // Otherwise set the appropriate mark.
+    // if counter reaches 9 without a winner, then it's a tie.
+    } else if (counter === 9) {
+      catsGame();
     }
-    clicked(e);
   }
 
-  // 
-  // 
+  // Element -> Boolean
+  // Check each of the winnning situations to see if they all match the textContent of the consumed element
   function match(element) {
-    for (var i = 0, len = grid.length; i < len; i++) {
-      grid.item(i);
+    // double loop through each to see if they have matching textConent.
+    for (var i = 0, j = 0, len = wins.length; i < len; i++) {
+      var text1 = document.querySelector(wins[i][j]).textContent;
+      var text2 = document.querySelector(wins[i][j + 1]).textContent;
+      var text3 = document.querySelector(wins[i][j + 2]).textContent;
+      // if true, then last player clicked wins
+      // i.e. if there is a match of three on any inner winning situations, call the winner function.
+      
+      if ((text1) && (text1 === text2 && text2 === text3)) {
+        winner(element);
+      }
     }
+  }
+
+  // Element -> Void
+  // Displays the winner of the game, and optionally resets the game.
+  function winner (element) {
+    alert("Player " + element.textContent + " wins!");
+    reset();
+  }
+
+  // Void -> Void
+  // alerts players of a tie game, and calls a reset if another is confirmed.
+  function catsGame() {
+    alert("Cats Game!");
+    reset();
   }
 
   // Node -> Void
@@ -63,6 +100,8 @@ var main = function main() {
     pTag.setAttribute("class", "bigX");
     pTag.textContent = "X";
     node.target.appendChild(pTag);
+    banner.setAttribute("class", "oBanner");
+    banner.textContent = "It's O's turn!";
     counter++;
   }
 
@@ -73,30 +112,29 @@ var main = function main() {
     pTag.setAttribute("class", "bigO");
     pTag.textContent = "O";
     node.target.appendChild(pTag);
+    banner.setAttribute("class", "xBanner");
+    banner.textContent = "It's X's turn!";
     counter++;
   }
 
-  // Void -> Void
-  // alerts players of a tie game, and calls a reset if another is confirmed.
-  function catsGame() {
-    alert("Cats Game!");
-    var confirmation = confirm("Play again?");
-    if (confirmation) {
-      reset();
+  // reset the counter when the 'reset' button is clicked, & when the game ends.
+  function reset() {
+    var element;
+
+    // set counter back to 0
+    counter = 0;
+
+    // set banner back to start text
+    banner.textContent = "It's X's turn!";
+
+    // double loop through each to see if they have matching textConent.
+    for (var i = 0, len = wins.length; i < len; i++) {
+      for (var j = 0, length = wins[i].length; j < length; j++) {
+        element = document.querySelector(wins[i][j]);
+        element.textContent = "";
+      }
     }
   }
-
-  // reset the counter when the 'reset' button is clicked, & when the game ends.
-
-    // 1.4) check to see if there are three 'Xs' or 'Os' relative to element just clicked, or if there are no more spaces left to click.
-      // 1.4.1) if true, pop up an option box with text of the winning player and asking if they want to play again.
-      // 1.4.2) option box buttons should say 'yes' and 'no'.
-
-  // b) check for three in a row or no more places to mark
-
-  // c) Game over option box
-
-  // d) reset page and counter
-};
+}
 
 window.onload = main;
