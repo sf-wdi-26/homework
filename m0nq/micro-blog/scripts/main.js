4659f1b54bed1388e764fa57213810da1f7baddf
m0nq <monq.wellington@gmail.com>
2016-01-26 22:01:12 -0800

    add commented plan to main.js

---
+++ m0nq/micro-blog/scripts/main.js
@@ -0,0 +1,9 @@
+window.onload = function main() {
+  // when the button is clicked
+    // prevent the default event behavior
+    // store the input from the text field
+    // reset the textfield
+    // create a new li object
+    // place the stored text field info into it's innerHTML
+    // prepend the li object above the already existing one.
+};
