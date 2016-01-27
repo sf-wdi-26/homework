052997560f26bb867d454454973b1efd85d872cc
Beta-23 <alfred.dominguez@gmail.com>
2016-01-26 20:36:02 -0800

    Adds app js code

--- Beta-23/micro-blog-w02-d02/app.js
+++ Beta-23/micro-blog-w02-d02/app.js
@@ -1,2 +1,24 @@
+
+
+var distance;
+var refNode;
+
 window.onload = function() {
+    console.log("linked");
+    document.getElementById("invia").addEventListener("click", catchelement);
+
 };
+
+function createElement () {
+        var $li = document.createElement("li");
+    $li.textContent = distance;
+    document.getElementById("Post-list").insertBefore($li, refNode);
+    refNode = $li;    
+}
+
+function catchelement(){
+
+     distance = document.getElementById("comment").value;
+     createElement();
+     
+	}
