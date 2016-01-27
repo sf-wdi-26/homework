ef6d0dc57f959bf4061df318ba6ced5e96e60240
cindyytong <tongcindyy@gmail.com>
2016-01-27 11:12:53 -0800

    fixes removal of text in input box

--- cindyytong/w02-d01-blog/js/app.js
+++ cindyytong/w02-d01-blog/js/app.js
@@ -6,24 +6,30 @@ window.onload = function(){
     
     function submitPost(e){
         //do something to push post to bottom of page
-        console.log("working");
     
         var getName = getId("name").value;
         var getHometown = getId("hometown").value;
         var getCity = getId("city").value; 
         var getStory = getId("story").value;
+//
+//       var posts = [];
+//        posts.push(getStory);
+//        getId("insertStory").innerHTML=posts
 
-        console.log(getName);
-        console.log(getHometown);
-        console.log(getCity);
-        console.log(getStory);
-        
         var posts = [];
         posts.push(getStory);
         getId("insertStory").innerHTML=posts
     }
     
-    getId("submit").addEventListener('click', submitPost)
+    //because we are resetting the value, we can't just set this equal to the variable because the varibale is not the node itself although it points to it; this will just reset the value of the variable
+    function reset(e){
+          getId("story").value = ""; 
+    }
+    
+    getId("submit").addEventListener('click', submitPost);
+    
+    getId("submit").addEventListener('click', reset);
 }
 
 //Find out how to include username and city along with the story
+
