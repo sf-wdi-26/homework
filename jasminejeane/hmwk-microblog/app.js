1ff52975989e026b71bb7a3bd51a64509df332f7
jasminejeane <jasminejeane@gmail.com>
2016-01-26 18:27:48 -0800

    added new files created new element and append to ul

---
+++ jasminejeane/hmwk-microblog/app.js
@@ -0,0 +1,60 @@
+
+
+window.onload = function() {
+
+//Sudo Code
+
+//grab form and create a variable for it
+//grab create button
+//create a from the button to the form
+//when clicked have form content append to ul
+//may have to create element
+//
+//
+
+
+
+
+// Your JS will hold an array of posts 
+var posts =[];
+var theform = document.getElementbyId("newPost");
+var submit = document.getElementsById("submit");
+var list = document.createElement("li");
+
+//function to append a new list item
+document.ul.appendChild(list);
+
+
+//that it appends to the HTML list.
+function appendPosts (){
+
+
+console.log("function on");
+
+}
+
+
+
+
+//When your user clicks submit,
+// your JS should handle taking the content of the form 
+ //and putting it in that array, and updating the DOM.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// body...
+};
+//windows closing tag
