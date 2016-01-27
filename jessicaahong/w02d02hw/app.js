75e09626ce82fa22d8770feff28cc4126c2eed85
jessicaahong <jessiehong9@gmail.com>
2016-01-26 22:28:11 -0800

    finished homework

--- jessicaahong/w02d02hw/app.js
+++ jessicaahong/w02d02hw/app.js
@@ -1,56 +1,54 @@
 window.onload = function() {
 var doNotSubmit = document.querySelector("#form");
-doNotSubmit.addEventListener("submit", function(event){
+doNotSubmit.addEventListener("submit", clickSubmit);
+addButtonsToExamplePost();
+ourTest();
+};
+
+function clickSubmit(event) {
 //stop default submit stuff
 	event.preventDefault();
 	var blogList = document.querySelector("#postingList");
 	var newPost = document.createElement("li");
+	var button = document.createElement("button");
 	newPost.setAttribute("class", "text");
-//push content of 
 	newPost.innerHTML = document.querySelector("#blurb").value;
 	blogList.appendChild(newPost);
 	document.querySelector("#blurb").value = "";
-});
+//add delete button for new blurbs and click to delete
+	button.setAttribute("class", "buttonclass");
+	button.innerHTML = "X";
+	button.setAttribute("id", "toDelete");
+	newPost.appendChild(button);
+	button.addEventListener("click", ourTest);
+}
 
-};
-
-
-
-// function deleteButton(){
-// posts = document.querySelectorAll("li");
-// for (i = 0; i < posts.length; i ++) {
-// 	var button = document.createElement("button");
-// 	button.setAttribute("class", "buttonclass");
-// 	posts[i].appendChild(button);
-
-// 	button.addEventListener("click", test);
-// }
-
-// function test() {
-// 	var blogList = document.querySelector("#postingList");
-// 	for (i = 0; i < posts.length; i ++){
-// 		blogList.removeChild();
-// 	}
-// }
-// // button.addEventListener("click", function(event){
-// // var blogList = document.querySelector("#postingList");
-// // for (i = 0; i < posts.length; i ++){
-// // 	blogList.removeChild(posts[i]);
-// // }
-// }
-// deleteButton();
-
-// };
+function addButtonsToExamplePost(){
+	posts = document.querySelectorAll("li");
+	for (i = 0; i < posts.length; i ++) {
+		var button = document.createElement("button");
+		button.setAttribute("class", "buttonclass");
+		button.innerHTML = "X";
+		posts[i].appendChild(button);
+	}
+}
 
 
-// //create button
-// //attach button to each li
+//yes, i put a click event listener in an iterator but i don't know 
+//how else to make this work when i already have existing sample blog posts!
+//it's still not working properly, either.
+//it takes two clicks then deletes the first post in the list, then the rest
+//disappear when you click any of the buttons.
 
-
-
-
-
-
-
-
+function ourTest() {
+	var blogList = document.querySelectorAll("li");
+	var button = document.querySelectorAll("button");
+	for (i = 0; i < blogList.length; i ++){
+		blogList[i].setAttribute("id","toDelete");
+		button[i].addEventListener("click", function(e) {
+			var post = document.querySelector("#toDelete");
+			post.parentNode.removeChild(post);
+		});
+	}
+}
 
