dd2c8ba8f4e978419b8cc1bf7e1feceb716a47b7
m0nq <monq.wellington@gmail.com>
2016-01-26 22:56:38 -0800

    refactor a predicate on an empty form field to complete the algorithm.

--- m0nq/micro-blog/scripts/main.js
+++ m0nq/micro-blog/scripts/main.js
@@ -1,9 +1,36 @@
 window.onload = function main() {
-  // when the button is clicked
+
+  // get submit button
+  var submitButton = document.getElementById('button');
+
+  submitButton.addEventListener("click", function (event) {
+    // when the button is clicked
     // prevent the default event behavior
+    event.preventDefault();
+
     // store the input from the text field
-    // reset the textfield
-    // create a new li object
-    // place the stored text field info into it's innerHTML
-    // prepend the li object above the already existing one.
+    var input = document.getElementById('text-box').value;
+
+    check(input);
+  });
+
+  function check (input) {
+    if (input) {
+      // reset the textfield
+      document.getElementById('text-box').value = '';
+
+      // create a new li object
+      var newListItem = document.createElement('li');
+
+      // place the stored text field info into it's innerHTML
+      newListItem.innerHTML = input;
+
+      // capture immediate child node of the ul
+      var firstChild = document.getElementById('main-list').children[0];
+
+      // append first child to the new list item
+      var parentNode = document.getElementById('main-list');
+      parentNode.insertBefore(newListItem, firstChild);
+    }
+  }
 };
