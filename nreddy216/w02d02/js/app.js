968041afc0cbfac632f1ee4e5603f5c8aede99cd
nreddy216 <nreddy216@gmail.com>
2016-01-26 18:36:37 -0800

    entry pushes to array and appends to list item in the unordered list

--- nreddy216/w02d02/js/app.js
+++ nreddy216/w02d02/js/app.js
@@ -1,4 +1,31 @@
 $(document).ready(function(){
+	console.log("Linked");
 
+	var submitBtn = $("#submit-btn");
+
+
+	submitBtn.click(function(event){
+		event.preventDefault();
+		storePosts();
+	});
+
+	var postArray = [];
+
+	function storePosts(){
+		var entry = $("#entry-box").val();
+
+		postArray.push(entry);
+
+		console.log(entry);
+		console.log(postArray);
+
+
+		var ul = $(".posts");
+		var li = document.createElement("li");
+
+		ul.append(li);
+
+		li.innerHTML = postArray[postArray.length-1];
+	}
 
 });
