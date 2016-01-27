8b15d2a7f9126836117992152015c1549d39eb3e
Michael Norelli <mnorelli360@gmail.com>
2016-01-26 20:45:23 -0800

    Fixes js to correctly get elements, add listener, and append posts

--- mnorelli/w02d02/js/main.js
+++ mnorelli/w02d02/js/main.js
@@ -1,5 +1,28 @@
 console.log("Linked.");
-window.onload = {
+window.onload = function(){
+
+setListener();
+
+};
+//pseudocode
+//
+// 
+// set up variables for form and tags
+// listen for submit, pass the result to a new child list element of the display
 
 
+function setListener () {
+  var tag = document.getElementById("tag");
+  tag.addEventListener("click", addPost);
 }
+
+  function addPost(e) {
+    console.log("working");
+    var posts = document.getElementById("posts");
+    var form = document.getElementById("form");
+    e.preventDefault();
+    var li = document.createElement("li");
+    li.textContent=tagPost.value;
+    posts.appendChild(li);
+  }
+
