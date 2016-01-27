f0487ca361545ad357f53fad0f71eb3daea27285
dchiamp <derek.chiampas@gmail.com>
2016-01-27 08:49:53 -0800

    w02d02 hw

--- dChiamp/w01d04/ticTacToe/app.js
+++ dChiamp/w01d04/ticTacToe/app.js
@@ -51,14 +51,26 @@ document.onclick = function getElIdFn (id) {
 }
  
 function getSquareValue (key) {
-		console.log(squareValues.key);
-		console.log(squareValues.a1);
+		console.log(squareValues[key]);
 }
+
 
 for (key in squareValues) {
 	console.log(squareValues[key]);
 }
 */
+
+
+// window.
+
+// event linter on click
+// return document.getele by id 
+
+// use this
+
+
+
+
 
 // winnging combos
 var winningValues = [7, 56, 73, 84, 146, 273, 292, 448];
@@ -67,28 +79,42 @@ var winningValues = [7, 56, 73, 84, 146, 273, 292, 448];
 // need to refactor so that i can have a function
 // to get the table id on click and then add event listener to that
 // Then iterate through the square values 
-a1.addEventListener("click", function() {
-	if (playerTurn === 0 ) {
-		this.textContent = playerXMove;
-		this.style.color = "red";
-		playerTurn = 1;
-		// need to find a way to get the val
-		// for any square in squareValues obj:
-		playerXTotalPoints += squareValues.a1;
-		console.log(squareValues.a1);
-		checkWin(playerXTotalPoints, playerX);
 
-	} else if (playerTurn == 1) {
-		this.textContent = playerOMove;
-		playerTurn = 0;
-		playerOTotalPoints += squareValues.a1;
-		checkWin(playerOTotalPoints, playerO);
-	}
+document.addEventListener("click", function(e) {
+	// var cell = this.getElementById(e.target.id);
+	var cellId = e.target.id;
+	console.log(cellId);
 
-	totalNumberOfTurns += 1;
-	checkTotalTurns(totalNumberOfTurns);
+
+	a1.addEventListener("click", function() {
+		if (playerTurn === 0 ) {
+			this.textContent = playerXMove;
+			this.style.color = "red";
+			playerTurn = 1;
+			// need to find a way to get the val
+			// for any square in squareValues obj:
+
+			// playerXTotalPoints += squareValues.a1;
+
+			playerXTotalPoints = squareValues.a1;
+			console.log(playerXTotalPoints);
+			console.log(squareValues.cellId);
+			checkWin(playerXTotalPoints, playerX);
+
+		} else if (playerTurn == 1) {
+			this.textContent = playerOMove;
+			playerTurn = 0;
+			playerOTotalPoints += squareValues.a1;
+			checkWin(playerOTotalPoints, playerO);
+		}
+
+		totalNumberOfTurns += 1;
+		checkTotalTurns(totalNumberOfTurns);
+	})
 })
 
+
+/*
 a2.addEventListener("click", function() {
 	if (playerTurn == 0 ) {
 		this.textContent = playerXMove;
@@ -175,7 +201,6 @@ b3.addEventListener("click", function() {
 
 	} else if (playerTurn == 1) {
 		this.textContent = playerOMove;
-		this.style.color = "red";
 		playerTurn = 0;
 		playerOTotalPoints += squareValues.b3;
 		checkWin(playerOTotalPoints, playerO);
@@ -240,28 +265,25 @@ c3.addEventListener("click", function() {
 	checkTotalTurns(totalNumberOfTurns);
 })
 
+*/
+
 // helper functions
 
 // check win by checking total value, and player
 function checkWin (value, player) {
 // should be able to iterate through winningValues array instead of:
-		if (value == 7  ||
-			value == 56 || 
-			value == 73 ||
-			value == 84 ||
-			value == 146 ||
-			value == 273 ||
-			value == 292 ||
-			value == 448 ) {
+	for (var i = 0; i < winningValues.length; i++) {
+		if (value == winningValues[i]) {	
 			winningPlayer = player;
 			alert(winningPlayer + " wins!");
-			// totalWins(winningPlayer);
+			totalWins(winningPlayer);
 		} else if (value == 511) {
 			alert("cat's game!")
-		} else {
-			// keep playing
-		}
-
+		} 
+		// else {
+		// 	keep playing
+		// }
+	};
 }
 
 // if no player matches winning number
@@ -274,38 +296,40 @@ function checkTotalTurns (num) {
 
 // bonus
 
-// total wins
+// scoreboard
+// total wins function
+var xScoreBoard = document.getElementById("x-total-wins");
+var oScoreBoard = document.getElementById("o-total-wins");
 
-// var xScoreBoard = document.getElementById("x-total-wins");
-// var oScoreBoard = document.getElementById("o-total-wins");
-// function totalWins(winningPlayer) {
-// 	if (winningPlayer == playerX) {
-// 		totalNumberOfTurns = 0
-// 		playerXTotalPoints = 0; 
-// 		playerXTotalWins += 1;
-// 		xScoreBoard.textContent = "Player X Wins: " + playerXTotalWins;
-// 		console.log(playerXTotalWins);
-// 		console.log(playerXTotalPoints);
-
-// 	} else if (winningPlayer == playerO){
-// 		totalNumberOfTurns = 0
-// 		playerOTotalPoints = 0;
-// 		playerOTotalWins += 1;
-// 		oScoreBoard.textContent = "Player O Wins: " + playerOTotalWins;
-// 	}
-
-// }
+function totalWins(winningPlayer) {
+	if (winningPlayer == playerX) {
+		playerXTotalWins += 1;
+		xScoreBoard.textContent = "Player X Wins: " + playerXTotalWins;
+	} else if (winningPlayer == playerO){
+		playerOTotalWins += 1;
+		oScoreBoard.textContent = "Player O Wins: " + playerOTotalWins;
+	}
+		// reset board
+		totalNumberOfTurns = 0
+		playerXTotalPoints = 0; 
+		playerOTotalPoints = 0;
+		playerTurn = 0;
+		console.log(playerXTotalWins);
+		console.log(playerXTotalPoints);
+		console.log(totalNumberOfTurns);
+}
 
 // reset button
 
 var resetButton = document.getElementById('reset');
 resetButton.addEventListener("click", function reloadPage (){
-	window.location.reload();
+	// window.location.reload();
 
 	// bonus
-	// var cellArray = [a1,a2,a3,b1,b2,b3,c1,c2,c3];
-	// for (i=0; i<cellArray.length; i++) {
-	// 	cellArray[i].textContent = "";
-	// }
+	var cellArray = [a1,a2,a3,b1,b2,b3,c1,c2,c3];
+	for (i=0; i<cellArray.length; i++) {
+		cellArray[i].style.color = "grey";
+		cellArray[i].textContent = "";
+	}
 })
 
