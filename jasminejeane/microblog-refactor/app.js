c3127ec24546f93777ae24a00af9c63c46b799bd
jasminejeane <jasminejeane@gmail.com>
2016-01-27 21:12:02 -0800

    linked js from prev hw to current blog.. trying to get features worked out w/ bootstrap

---
+++ jasminejeane/microblog-refactor/app.js
@@ -0,0 +1,122 @@
+
+
+
+
+//Sudo Code
+
+//grab form and create a variable for it
+//grab create button
+//create a from the button to the form
+//when clicked have form content append to ul
+//may have to create element
+//
+//
+
+
+
+
+// Your JS will hold an array of posts 
+
+var theform = document.getElementById("newPost");
+var submit = document.getElementById("theButton");
+
+// var list = document.createElement("li");
+
+//function to append a new list item
+// document.ul.appendChild(list);
+
+
+//that it appends to the HTML list.
+function appendPosts(event){
+event.preventDefault();
+var info = document.getElementById("info").value;
+var text = document.createTextNode(info);
+var list = document.createElement("li");
+list.appendChild(text);
+document.getElementById("appendMe").appendChild(list);
+reset();
+
+}
+
+
+submit.onclick = appendPosts;
+
+
+
+function reset(){
+	 document.getElementById("info").value = "";
+
+
+}
+
+//wrote this just to see if my event listener was working
+function checker(){
+	console.log("was clicked");
+
+}
+
+submit.onclick = appendPosts;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//prevent Default Function
+// function stopDefAction(evt) {
+//     evt.preventDefault();
+// }
+
+//was having alot of trouble with code below 
+
+// document.getElementById("submit").addEventListener("submit", function(){
+// 	stopDefAction();
+// 	appendPosts();
+// 	console.log("working");
+
+// });
+
+
+
+//When your user clicks submit,
+// your JS should handle taking the content of the form 
+ //and putting it in that array, and updating the DOM.
+
+
+
+//remember to add preventDefault
+
+
+
+//problems
+//having trouble with event listener
+//had to change button from submit to button type. 
+//Going to try with submit later add adding on prevent default 
+
+
+
+
+
+
+
+
+
+	// body...
+
+//windows closing tag
