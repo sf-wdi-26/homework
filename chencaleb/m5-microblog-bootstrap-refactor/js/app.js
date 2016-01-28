996ecf10e845c03507c6ebdd6bbfe59996462799
chencaleb <clb_chen@berkeley.edu>
2016-01-27 23:51:07 -0800

    refactors jQuery

--- chencaleb/m5-microblog-bootstrap-refactor/js/app.js
+++ chencaleb/m5-microblog-bootstrap-refactor/js/app.js
@@ -1,5 +1,17 @@
 window.onload = function() {
 
+$("#postButton").on("click", function(event) {
+event.preventDefault();
+
+
+var newPost = $("<li></li>").attr("class", "text");
+var oldPosts = $("#postList");
+
+//newPost.
+
+});
+
+
 var submitButton = document.getElementById("postButton");
 submitButton.addEventListener("click", function(event) {
 
