3e46aa774b462fc125b6599f40085647c999d491
Jehnean <jehnean@gmail.com>
2016-01-29 09:26:46 -0800

    Minor updates to JS

--- Jehnean/m4-ajax-lab/base.js
+++ Jehnean/m4-ajax-lab/base.js
@@ -1,23 +1,20 @@
-var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
+var weeklyQuakesEndpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
 var map;
 // Your short term goal is to render each title to the "info" section of the page (see the commented example in your HTML)
-
-// QUESTION -- The $ at the beginning of this is the one thing I really don't get. Is this a jQuery thing I missed? 
-var $info_row_target;
+var addToHTML;
 
 // What is the structure of the data? "features": --> "type", "properties", "geometry", "id"
 // How many earthquakes does it list? "count": 3
-// How would you grab the first earthquake? From weekly_quakes_endpoint, I think I need to access "features" [0]. I can also use weekly_quakes_endpoint .properties."THING" to get to things. 
+// How would you grab the first earthquake? From weeklyQuakesEndpoint, I think I need to access "features" [0]. I can also use weeklyQuakesEndpoint .properties."THING" to get to things. 
 // How would you grab it's title? features[0].properties.title
 // How would you grab it's geological coordinates: latitude? coordinates[1] longitude? coordinates[0]
 // When did it happen? properties.time
-
 // My Google API Key: AIzaSyC6x1Yk7HCuQ6RFEWNvxI0XFTnInz8sW9k
 
 // On page load display google map and get USGS info 
 $(document).ready(function(){
 
-    $info_row_target = $("#info");
+    var addToHTML = $("#info");
 
         //Part 2. Your next goal is to integrate Google Maps:
         map = new google.maps.Map(document.getElementById('map'), {
@@ -27,12 +24,12 @@ $(document).ready(function(){
         zoom: 1
     });
 
-    $.get(weekly_quakes_endpoint, function(response_data){
+    $.get(weeklyQuakesEndpoint, function(response_data){
         // Loop over it - for loop or forEach loop
         response_data.features.forEach(function i(quakeInfo) {
         // Add each title to the page
         var title = quakeInfo.properties.title; 
-        $info_row_target.append( "<p>" + title + "</p>");
+        addToHTML.append( "<p>" + title + "</p>");
 
         // Part 3. Add pins to your map
         /* your next goal is to drop a single pin on San Francisco
