41337f8d5dfcfd44df27d97c57a27e655367e270
jasminejeane <jasminejeane@gmail.com>
2016-01-29 09:13:50 -0800

    homework w01 do4

--- jasminejeane/m4-ajax-lab/starter-code/base.js
+++ jasminejeane/m4-ajax-lab/starter-code/base.js
@@ -15,21 +15,26 @@
 // When did it happen?
 // How many hours ago is that?
 
-var firstQuake = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us20004vbk.geojson";
-$.get(firstQuake, function(response_data) {
 
-	window.newData = response_data;
-	for (var key in window.newData){
-	if (window.newData.hasOwnProperty(key)) {
-    var val = window.newData[key];
-    console.log(val);
-    
-  // creating a global variable to inspect the new data is good
-  // just remember to make it local once your done inspecting!
-  // window.newData = response_data;
-}
-}
-});
+
+// what I got to work on my own
+
+// var firstQuake = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us20004vbk.geojson";
+// $.get(firstQuake, function(response_data) {
+
+// 	window.newData = response_data;
+// 	for (var key in window.newData){
+// 	if (window.newData.hasOwnProperty(key)) {
+//     var val = window.newData[key];
+//     console.log(val);
+//     // console.log(val.Object);
+
+//   // creating a global variable to inspect the new data is good
+//   // just remember to make it local once your done inspecting!
+//   // window.newData = response_data;
+// }
+// }
+// });
 
 // function locateKey(info){
 // for (var key in window.newData){
@@ -39,6 +44,36 @@ $.get(firstQuake, function(response_data) {
 //   }
 // }
 // }
+
+
+
+
+
+
+var $info_row_target;
+var map;
+
+$(document).ready(function(){
+
+  $info_row_target = $("#info");
+  //render a map
+ 
+  //grab the data
+  fetchQuakeData();
+
+});
+var firstQuake = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us20004vbk.geojson";
+
+function fetchQuakeData(){
+  $.get(firstQuake, function(response){
+    response.type.features.forEach(function renderRowAndMarker(quake){
+      // Add title of the quake
+      var title = quake.properties.title;
+      console.log(title);
+  });
+
+});
+ }
 
 
 
