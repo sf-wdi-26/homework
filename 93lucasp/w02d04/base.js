e432e70bd7c08d390817e067077aa6d0ce2331ac
93lucasp <spezzanoluca@gmail.com>
2016-01-29 10:04:14 -0800

    changes to w02d04

--- 93lucasp/w02d04/base.js
+++ 93lucasp/w02d04/base.js
@@ -1,21 +1,24 @@
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
 var title;
 var map;
-var where;
 var time;
 
 $(document).ready(function(){
 
   map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 39.3, lng: -74.3},
+    zoom: 2 
+   });
+  /* map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.78, lng: -122.44},
     zoom: 12
   });
  
- var marker = new google.maps.Marker({
+ /*var marker = new google.maps.Marker({
     position: {lat: 37.78, lng: -122.44},
     map: map,
     
-  });
+  });*/
 
 
 $.get(weekly_quakes_endpoint, function(response_data) {
@@ -23,14 +26,15 @@ $.get(weekly_quakes_endpoint, function(response_data) {
   newData = response_data;
   
   var earth = newData.features;
-  console.log(earth);
+
   for (var i = 0; i < earth.length; i++) {
   	title = newData.features[i].properties.title;
   	time = newData.features[i].properties.time;
   	$("#info").append("<h1>" +title+ "</h1>");
   	var d = new Date();
-  var n = d.getTime()- time;
-   $("#info").append("<h2>"+"It was was " +n+ "milliseconds ago"+ "</h2><br />");
+  
+  var n = Math.round( ( Date.now() - newData.features[i].properties.time ) / (1000*60*60) );
+   $("#info").append("<h2>"+"It was was " +n+ " hours ago"+ "</h2><hr> ");
   
 
   	var lng1 = newData.features[i].geometry.coordinates[0];
