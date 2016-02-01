c287c6bd6d0c78f64e9c63f41e91bdcab64a4ae4
takshingli810 <takshingli810@gmail.com>
2016-02-01 09:53:25 -0800

    w03d01HW

--- takshingli810/w02d02HW/app.js
+++ takshingli810/w02d02HW/app.js
@@ -1,7 +1,8 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
 
 
-window.onload = function(){
-console.log("Linked");
+
 //1. Your HTML page should have a list in it that will display the posts. 
 //It can be empty, with JS adding posts, or have some dummy posts in it to start.
 //2.Your JS will hold an array of posts that it appends to the HTML list.
@@ -12,76 +13,42 @@ console.log("Linked");
 // push onto array list
 // apprendChild into li for post
 
-//Prevent Refreshing
-document.getElementById("typing").addEventListener("submit", getInput);
+
 
 //variable
-var userInput = [];
-//var posts = [];
+var userArray = [];	
+var $myForm = $("#myForm");
 
-function getInput(event) {
-event.preventDefault(); 
-userInput = document.getElementById("input");
-console.log('clicked');
-console.log(userInput);
+$('#clear').on('click', reset);
 
-var newAnswer = document.createElement('li');
-newAnswer.innerHTML = name.value;
+$myForm.on("submit", function(e) {
+	e.preventDefault(); //Prevent Refreshing
+	console.log('clicked'); //log 
+	if(localStorage.clickCount){
+				localStorage.clickCount = Number(localStorage.clickCount)+1;
+				$("#result").html("You have clicked the button " + localStorage.clickCount + " time(s).");
+			}else{
+				localStorage.clickCount = 1;
+				$("#result").html("You have clicked the button " + localStorage.clickCount + " time(s).");
+			}
+	getInput();
+	$('#typing').val(' ');
+} );
 
-unordered.appendChild(newAnswer);
-this.reset();
+function getInput(){
+	var $uL = $("#uL");
+	var $userInput = $('#typing').val();
+	userArray.push($userInput);
+	console.log(userArray);
+	$('uL').append("<li>"+$userInput+"</li>");// put li into ul
+	} 
+		
 
 
-
+function reset() {
+	localStorage.clear();
 }
-};
-
-/**
-window.onload = function () {
-
-console.log('linked');
-//set global variables so it can be accessed in all functions
-// var name = {
-
-// };
-var userInput = [];
-var userPost;
 
 
-//create a var for form block
-//create a var for ul block
-var $postButton = document.getElementById("postButton");
-var $myForm = document.getElementById("myForm");
-var $ul = document.getElementById("ulBox");
-//add event listener "preventDefault" so default action will not occur
-//set var userPost to get userText & value
-//add userPost to userInput array.
-
-$postButton.addEventListener("click", function(event) {
-    event.preventDefault()
-    userPost = document.getElementById("userText").value;
-    // if (userPost !== false ) {
-        // console.log(userPost)
-        getInput();b
-    // } else {
-    //     alert('You cannot submit an empty box');
-    // }
-    userInput.push(userPost);
-    console.log(userInput);
-    // getInput();
-    $myForm.reset();
 });
 
-
-//create newLi using js
-//append newLi to ul tag
-//set li to equal what user inputs.
-function getInput() {
-    var $newLi = document.createElement("li");
-    $ul.appendChild($newLi);
-    $newLi.textContent = userPost;
-}
-
-};```
-
-[*/
