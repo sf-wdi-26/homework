bba41ea1d744a6dbc728512cbe9e5b9976c60069
chencaleb <clb_chen@berkeley.edu>
2016-01-31 23:58:03 -0800

    adds delete function

--- chencaleb/m5-microblog-bootstrap-refactor/js/app.js
+++ chencaleb/m5-microblog-bootstrap-refactor/js/app.js
@@ -12,12 +12,47 @@ if(sessionStorage.clickCount){
 
 var oldPosts = $("#postList");
 var newPost = $("<li></li>").attr("class", "text");
+// var deleteText = document.createElement("button");
 
 var inputText = $("#textBox").val();
 newPost.html(inputText);
 oldPosts.append(newPost);
 $("#textBox").val("");
+// deletePost.setAttribute("class", "removeButton");
+// newPost.appendChild(deletePost);
+
+// newPost.append(deleteText);
+// deleteText.attr("class", "deleteButton");
+// deleteText.addEventListener("click", removeLi);
+
+
 });
+
+deleteButton();
+
+};
+
+
+//creates delete button (does not work correctly)
+
+function deleteButton () {
+	posts = document.querySelectorAll("li");
+	for(var i=0; i<posts.length; i++){
+		var deletePost = document.createElement("button");
+		deletePost.setAttribute("class", "removeButton");
+		posts[i].appendChild(deletePost);
+		deletePost.addEventListener("click", removeLi);
+	}	
+}
+
+function removeLi () {
+	var blogList = document.querySelectorAll("li");
+	for(var i=0; i<blogList.length; i++){
+		blogList[i].setAttribute("id", "toDelete");
+		var deleted = document.querySelector("#toDelete");
+		deleted.remove();
+	}
+}
 
 
 //CODE BELOW IS IN JAVASCRIPT
@@ -36,4 +71,3 @@ $("#textBox").val("");
 // document.getElementById("textBox").value = "";
 // });
 
-};
