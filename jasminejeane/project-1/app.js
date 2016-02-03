93af85b9dac532937d5affa08e5a90f5bc0cc0ea
jasminejeane <jasminejeane@gmail.com>
2016-02-03 12:35:13 -0800

    added draggablr feature to puzzle

---
+++ jasminejeane/project-1/app.js
@@ -0,0 +1,26 @@
+//convert picture into a puzzle
+// copy each peace as a individual picture 
+
+//copy orginal layout of picture
+//copy layout that looks like the puzzle
+//for each puzzle piece create an id
+
+
+//make copy that's pre cut invisible or grid it out so that those spaces will recognize when the right picture 
+//is on it
+		//if id a == id a1 that picture cam be submitted kind of like if the input has a certain amount of characters
+		// this the input is ok
+		 	//else __ something
+
+//studing drag and drop
+// reasearch snap.js
+
+
+
+function init() {
+
+  $("#drag1").draggable();
+	//do stuff on page load...
+  //set event listeners maybe
+}
+
