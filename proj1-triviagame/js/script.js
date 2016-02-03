785ca3cdcbb62a24fb5392164ca397fd7899f834
cloverharvest <anna@schwabdev.com>
2016-02-03 12:35:48 -0800

    Adds starter questions, W03, wip

---
+++ proj1-triviagame/js/script.js
@@ -0,0 +1,15 @@
+alert('ready');
+
+//define function to wrap whole code
+//in it:
+// define variable called QUESTIONS 
+//Enumerate Question Set 1 hard coded onto same js file
+//OR 
+//load the JSON data from an external file
+
+//define variable QUESTIONCOUNTER which tracks question number
+//define variable USERANSWERS which is an empty array for now;
+//define variable QUIZ which is assigns the quiz div object
+
+
+//Display initial question
