48c39592e3a62f3314aafc31ae9257fc0e75c0f3
cindyytong <tongcindyy@gmail.com>
2016-02-03 14:14:27 -0800

    Meets with Ben fixes map to load on next button click

--- cindyytong/proximity-game/new/js/quest1.js
+++ cindyytong/proximity-game/new/js/quest1.js
@@ -1,76 +1,103 @@
 //MOVE TO SECOND PAGE when Start button is clicked
 
-//Initialize functions: generate random array of 10 numbers to call the bar locations from 
+//Initialize functions:
 $(document).ready(function(){
-    generateRandomArray();
+    google.maps.event.addDomListener(document.getElementById('next'), 'click', initialize);
 });
 
 
-//Randomly select a number from the array
-//this function generates an array of numbers 1 to 10 in a random order 
-function generateRandomArray(){
-  var arrRandomTen = [];
-  while(arrRandomTen.length<10){
-    var randomNumber = Math.ceil(Math.random()*10)
-    var found = false;
-    for(var i = 0; i<arrRandomTen.length; i++){
-      if(arrRandomTen[i]==randomNumber){
-        found=true; 
-        break};
-      }
-      if(!found)arrRandomTen[arrRandomTen.length]=randomNumber;
-    }
-    
-  console.log(arrRandomTen);
-}
-
-//III. GRAB BAR DATA - Based on current location, do search for nearby bars, return those results as var barResults = {} which is an object with data; run function once start is clicked 
 
 function initialize() {
-  var pyrmont = new google.maps.LatLng(37.78,-122.44);
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: pyrmont,
-    zoom: 15,
-    scrollwheel: false
-  });
+      var arrRandomTen = [];
+      function generateRandomArray(){
+        console.log("generateRandomArray called");
+        while(arrRandomTen.length<40){
+          var randomNumber = Math.ceil(Math.random()*40)
+          var found = false;
+          for(var i = 0; i<arrRandomTen.length; i++){
+            if(arrRandomTen[i]==randomNumber){
+              found=true; 
+              break};
+            }
+            if(!found)arrRandomTen[arrRandomTen.length]=randomNumber;
+          }
+          
+        return arrRandomTen;
+      }
+      generateRandomArray();
+      var generalAssembly = new google.maps.LatLng(37.790841,-122.401280);
 
-  // Specify location, radius and place types for your Places API search.
-  var request = {
-    location: pyrmont,
-    radius: '500',
-    types: ['bar']
-  };
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: generalAssembly,
+        zoom: 16,
+        scrollwheel: false,
+        
+      });
 
-  // Create the PlaceService and send the request.
-  // Handle the callback with an anonymous function.
-  
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, function(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      var place = results[0];
-      console.log("working");
-      // for (var i = 0; i < results.length; i++) {
-      //   var place = results[i];
-      //   // If the request succeeds, draw the place location on
-      //   // the map as a marker, and register an event to handle a
-      //   // click on the marker.
-      //   console.log(results[0].name);
-      //    if(results[0].name ==="BAR CRUDO"){
-      //     console.log("Correct")
-      //   }
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
-      // }
-    }
-  });
+      // Specify location, radius and place types for your Places API search.
+      var request = {
+        location: generalAssembly,
+        radius: '300',
+        types: ['restaurant']
+      };
+
+    //turn off labels from map
+      var stylesArray = [
+           {
+             featureType: "administrative",
+             elementType: "labels",
+             stylers: [
+               { visibility: "off" }
+             ]
+           },
+           {
+             featureType: "poi",
+             elementType: "labels",
+             stylers: [
+               { visibility: "off" }
+             ]
+           }
+         ];
+
+      map.setOptions({styles: stylesArray});
+
+      // Create the PlaceService and send the request.
+      // Handle the callback with an anonymous function.
+      
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch(request, function(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+
+          var place = results[arrRandomTen[0]];
+          //skips places that has geometry attribute of undefined
+          if(typeof(results[arrRandomTen[0]].geometry) === undefined){
+            var place = results[arrRandomTen[1]];
+          } 
+          console.log(results);
+
+          // for (var i = 0; i < results.length; i++) {
+          //   var place = results[i];
+          //   // If the request succeeds, draw the place location on
+          //   // the map as a marker, and register an event to handle a
+          //   // click on the marker.
+          //   console.log(results[0].name);
+          //    if(results[0].name ==="BAR CRUDO"){
+          //     console.log("Correct")
+          //   }
+            var marker = new google.maps.Marker({
+              map: map,
+              position: place.geometry.location
+            });
+          }
+        // }
+      });
 }
 
-// Run the initialize function when the window has finished loading.
-google.maps.event.addDomListener(window, 'load', initialize);
+//END OF FUNCTION INITIALIZE -------------------------------------
 
+// Run the initialize function when the window has finished loading.
+// google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
