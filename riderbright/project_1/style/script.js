e4583b22eb66e81ce48ea9ff40543645456f8f2c
Ryder Bright <ryderbright@gmail.com>
2016-02-03 21:05:55 -0800

    got timer working and fixing e.listeners

--- riderbright/project_1/style/script.js
+++ riderbright/project_1/style/script.js
@@ -2,10 +2,11 @@
 var canvas;
 var context;
 var bgImage;
+var bgReady;
 var redShipReady;
 
-
 window.onload = function(){
+    //var bounds = canvas.getBoundingClientRect();
     canvas = document.createElement("canvas");
     context = canvas.getContext("2d");
     canvas.width = 1200;
@@ -16,62 +17,91 @@ window.onload = function(){
     
     
 
-    console.log (canvas);
-    //construct background
+    console.log ("canvas");
     bgImage = new Image();
     bgImage.onload = function(){
-	   bgReady = true;
-       //drawGame used to add all images in start postion
-      // drawGame();
-    console.log("background loaded")
+	   bgReady = true;///
+        console.log("background loaded")
     };
-    
     bgImage.src = "sprites/canvas_1.png";
-    //background image placed
-    //next load other images
     
-    //need constructor function for other elements
-    var SpaceShip = function(x,y,speed,src){
-        //input unique to each ship w this.
-        //value to the redShip() & blueShip()
-        //and if SpaceShip.protoype.newAttribute 
-        //then u can add a shared attribute 
+    
+
+    var SpaceShip = function(x,y,speed,src,heart){
         this.x = x;
         this.y = y;
         this.speed = speed;
         this.src = src;
+        this.heart = heart;
         this.image = new Image();
         this.image.src = src;
-        // added image onload/
-        // learing more about add attributes 
-        // & instances 
         this.image.onload = function(){
             redShipReady = true;
             console.log ("red ready");
         };
 
-    };    //define variables w unique input
-    
-    var redShip = new SpaceShip(77,400,60,"sprites/spacey_1_one.png");
-        
-    //var blueShip = new SpaceShip(77,222,60,"sprites/spacey_2.png");
+    }; 
     
     
-    //spacebox contructor function
-    //var SpaceBox = function(x,y,speed,src){
-
-    //
-
-
-    //};
-
-
-    //define unique spaceBox
-    //var spaceBoxOne = new SpaceBox(1260,y,speed,src)
+    var redShip = new SpaceShip(100,450,100,"sprites/spacey_1_one.png",3);
+    var blueShip = new SpaceShip(100,250,100,"sprites/spacey_2_one.png",3);
+    var boxShip = new SpaceShip(1100,350,100,"sprites/spacey_box_1.png",0);    
+    
 
     console.log("elements ready");
-    //extra objects declared
+    //declare other extra game objects
+  
     
+    
+    var keysDown = {}
+        //add event
+        document.addEventListener("keydown",function(e){
+            keysDown[e.keycode==38||40||39||37] = true
+            console.log("keys down")
+        }, false);
+        
+        document.addEventListener("keyup",function(e){
+            delete keysDown[e.keycode==38||40||39||37];
+            console.log("keys down")    
+        }, false);
+        
+        console.log("listeners ready");
+    
+    var reset = function () {
+        
+    };
+    console.log("reset funk"); 
+
+//add switch function
+    var update = function (modifier){
+        if (38 in keysDown) { 
+            redship.y -= redShip.speed * modifier;
+        }
+        if (40 in keysDown) { 
+            redShip.y += redShip.speed * modifier;
+        }
+        if (37 in keysDown) { 
+            redShip.x -= redShip.speed * modifier;
+        }
+        if (39 in keysDown) { 
+            redShip.x += redShip.speed * modifier;
+        }
+       
+        console.log("updating")
+        
+        if (redShip.x <= (boxShip.x + 25)
+            && boxShip.x <= (redShip.x + 25)
+            && redShip.y <= (boxShip.y + 25)
+            && boxShip.y <= (redShip.y + 25)
+        
+        //   if (blueShip.x <= (boxShip.x + 25)
+        //    && boxShip.x <= (blueShip.x + 25)
+        //    && blueShip.y <= (boxShip.y + 25)
+        //    && boxShip.y <= (blueShip.y + 25)
+        ) { heartCount=1;
+            reset();
+        }
+    };
 
     var drawGame = function(){
         //if if checklist
@@ -80,18 +110,41 @@ window.onload = function(){
             console.log("elements on canvas");
         } 
         if (redShipReady){
-            context.drawImage(redShip.image,0,0)
+            context.drawImage(boxShip.image,boxShip.x,boxShip.y);
+            context.drawImage(redShip.image,redShip.x,redShip.y);
+            context.drawImage(blueShip.image,blueShip.x,blueShip.y);
+        
+            
             console.log("ship ready");
-        }
-    }
+        };    
+    };
 
-    //add event listeners,
-    //waits for 1000 milliseconds before running first input,
-    setTimeout(drawGame,1000);
+//gameClock
+// main is main game clock
+    var main = function () {
+        var now = Date.now();
+        var delta = now - then;
     
-
-}
+        update(delta / 1000);
+        drawGame();
+    
+        then = now;
+        console.log("tick tock")
+        
+        requestAnimationFrame(main);
+    };
+    //keeps timer more even through different browser
+    var w = window;
+    requestAnimationFrame = 
+    w.requestAnimationFrame || 
+    w.webkitRequestAnimationFrame || 
+    w.msRequestAnimationFrame || 
+    w.mozRequestAnimationFrame;
+    
+    var then = Date.now();
+    update();
+    main(); 
+};
       
-    
-
+            
       
