ab2e6a06d40c91b202944aea271b25b2c199ec8e
cloverharvest <anna@schwabdev.com>
2016-02-04 16:18:14 -0800

    Adds revised HTML, js, css, wip W03

--- proj1-triviagame/js/script.js
+++ proj1-triviagame/js/script.js
@@ -1,15 +1,55 @@
-alert('ready');
+//AS A  USER
 
-//define function to wrap whole code
-//in it:
-// define variable called QUESTIONS 
-//Enumerate Question Set 1 hard coded onto same js file
-//OR 
-//load the JSON data from an external file
+//Wait for first question
+//Give an answer
+//Press next button
+//Repeat 5 times
+//Get total score after 5 questions
 
-//define variable QUESTIONCOUNTER which tracks question number
-//define variable USERANSWERS which is an empty array for now;
-//define variable QUIZ which is assigns the quiz div object
+//AS A PROGRAMMER
+
+//Show questions one at a time using display: none or visibility
+//Get answer
+//Check for correctness
+//Start running score
+//Await Next button prompt 
+//show questions until all 5 questions are used
+// Show total after 5
+
+/*$(document).ready(function(){
+
+	submitAnswer();
+});*/
 
 
-//Display initial question
+
+
+
+
+function submitAnswer() {
+	total = 5;
+	score = 0;
+
+//Get User Input
+	var q1 = document.forms["quizbox"]["q1"].value;
+	var q2 = document.forms["quizbox"]["q2"].value;
+	var q3 = document.fomrs["quizbox"]["q3"].value;
+	var q4 = document.forms["quizbox"]["q4"].value;
+	var q5 = document.forms["quizbox"]["q5"].value;
+	alert(q1);
+
+//Validate Form
+ for (i = 0; i <= total; i ++) {
+ 	if eval("q" + i == null) || eval ())
+ }
+
+
+// Answer Key
+
+// Keep Score
+
+//Show Results
+
+
+
+
