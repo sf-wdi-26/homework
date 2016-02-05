0df4fd80ded4370eb3a0e9493f60b49dcb6b1e75
cloverharvest <anna@schwabdev.com>
2016-02-05 15:37:24 -0800

    W03 project wip

---
+++ proj1-triviagame/README.md
@@ -0,0 +1,7 @@
+README.md
+
+explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
+
+This is a short javascript quiz game using html, css, javascript, jquery. I took a conventional approach of adopting a user story, wireframing, workflow planning, defining functionality, and adjusting it to what my knowledge and skills can do. Still a work in progress.
+
+Unsolved problem is for the scoring function to work.
--- proj1-triviagame/js/script.js
+++ proj1-triviagame/js/script.js
@@ -1,83 +1,55 @@
 
-$(document).ready(function(){
-	addEventListeners();
-});
+/*$(document).ready(function() {*/
 
-function addEventListeners(){
 	
-	addListenerToNextButton();
-	addListenerToSubmitButton();
-
-}
-
-
-function addListenerToNextButton(){
-	$("#next").click(function() {
-		// debugger
-		var current = $(".qn:visible");
-		
-		total = $(".qn").length;
-		
-		last = $(".qn:last");
-		
-		$(current).next().show();
-		
-		$(current).hide();
-		
-		$(current).next().is(last){
-			$("#next").hide();
-			$("#prettysubmit").show();
-		}
-});		
-}
-
-function addListenerToSubmitButton(){
-	$("#prettysubmit").click(function(){
-		console.log("hi anna the submit button was clicked");
-	
+	function submitAnswers() {
 	
 		total = 5;
 		score = 0;
 
-	//Get User Input
+	//Get User Input	
 		var q1 = document.forms["quizbox"]["q1"].value;
 		var q2 = document.forms["quizbox"]["q2"].value;
 		var q3 = document.forms["quizbox"]["q3"].value;
 		var q4 = document.forms["quizbox"]["q4"].value;
 		var q5 = document.forms["quizbox"]["q5"].value;
 		
-
+	 ((q1 === null) || (q1 = " ")){
+		alert("You missed question 1");
+	}
+};
+	
+	/*
 	//Validate Form
 
-		for(i = 1; i <= total; i++) {
-			if ( (eval("q" + i) === null) || (eval("q" + i) === "") ){
+	for(i = 1; i <= total; i++) {
+			if ( (eval("q" + i) === null) || (eval("q" + i) = " ") ){
 			alert("You missed question " + i);
-			return false;
+			
 		}
 	}
+}
+
 
 	//Make answer key
 
-	var answers = ["a", "c", "d", "b", "c"];
+	var answers = ["a", "d", "d", "b", "c"];
 
 	//Check answers
 
 	for(i = 1; i <= total; i++) {
-		if (eval("q" + i) === answers[ i - 1]) {
+		if (eval("q" + i) === answers[i - 1]) {
 			score++;
 		}
 	}
 
-	var results = documentGetElementById('results');
+	//Display results
+
+	var results = documentGetElementById("results");
 	results.innerHTML = "<h3>You scored" +score+ "out of" +total+"</h3>";
-	alert("You scored " + score + " out of " + total);
+	alert("You scored " +score+ " out of " +total+ ". That is a start, keep on training!");
 
 	return false;
 
-
-});
-
 }
-
-
-
+}); */
