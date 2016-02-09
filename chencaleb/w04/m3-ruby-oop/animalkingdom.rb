fcff8cddaa3f24513f3bfab7ea0ef467a3946a3b
chencaleb <clb_chen@berkeley.edu>
2016-02-08 21:47:32 -0800

    adds comments

--- chencaleb/w04/m3-ruby-oop/animalkingdom.rb
+++ chencaleb/w04/m3-ruby-oop/animalkingdom.rb
@@ -2,18 +2,23 @@ class Animal
 	def initialize(arg)
 		@type = arg
 	end
+	#eat method
 	def eat(food)
 		p "I am eating #{food}."
 	end
+	#sleep method
 	def sleep
 		@state = "asleep"
 	end
+	#wake method
 	def wake
 		@state = "wake"
 	end
 end
 
+#person inherits animal
 class Person < Animal
+	#getter/setter
 	attr_accessor :age, :gender, :name
 	def initialize(age, gender, name)
 		@type = "person"
@@ -21,6 +26,8 @@ class Person < Animal
 		@gender = gender
 		@name = name
 	end
+
+	#eat method
 	def eat(food)
 		if food == "person"
 			p "You can't eat other people!"
