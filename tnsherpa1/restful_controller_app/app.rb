902c5d20c4ff089ea0548aa4b813d7966903fe19
tnsherpa1 <tnsherpa1@gmail.com>
2016-02-10 08:56:48 -0800

    adds views

--- tnsherpa1/restful_controller_app/app.rb
+++ tnsherpa1/restful_controller_app/app.rb
@@ -2,6 +2,16 @@ class RestfulControllerApp < Sinatra::Base
 get "/" do 
 	erb "This is my home page"
 end
+get "/about" do
+	erb :'about'
+end
+get "/contact" do
+	erb :'contact'
+end
+get "/products" do
+	erb :'products'
+end
+
 
 
 end
