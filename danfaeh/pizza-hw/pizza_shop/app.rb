7d46413d9e10b5c5403f01f5cd05aca18798efbf
danfaeh <danfaeh@gmail.com>
2016-02-12 09:04:18 -0800

    pizzashop migrations

--- danfaeh/pizza-hw/pizza_shop/app.rb
+++ danfaeh/pizza-hw/pizza_shop/app.rb
@@ -1,2 +1,45 @@
 class PizzaShop < Sinatra::Base
+  
+  # home
+  get "/" do 
+    erb :home
+  end
+
+  # index
+  get "/pizzas" do
+    @pizzas = Pizza.all
+    erb :index
+  end
+
+  # show
+  get "/pizzas/:id" do
+    @pizza = Pizza.find(params[:id])
+    erb :show
+  end
+
+  # # new
+  # get "/pizzas/new" do
+  #   @pizza = Pizza.new
+  #   erb :"pizzas/new"
+  # end
+
+  # # create
+  # post "/pizzas" do
+  #   @pizza = Pizza.new(params[:pizza])
+  #   if @pizza.save
+  #     redirect :index
+  #   else
+  #     erb :index
+  # end
+
+
+  # # update
+  # puts "/" do
+  # end
+
+
+  # # delete
+  # get "/"
+  # end
+
 end
