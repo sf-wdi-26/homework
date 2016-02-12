9abbe6066dde8560f06b9cb0419b8a8b4198c443
Vien Van <vienvonvan@gmail.com>
2016-02-12 08:56:54 -0800

    Got pizza to work

--- VienVan/hw-w04-d04/pizza_shop/app.rb
+++ VienVan/hw-w04-d04/pizza_shop/app.rb
@@ -1,12 +1,60 @@
 class PizzaShop < Sinatra::Base
 
-  get '/' do
-    erb :'index'
-  end
+    get '/' do
+        erb :home
+     end
 
-  get '/pizzas' do
-    @pizzas = Artist.all
-    erb(:"pizzas/index")
+#get all pizzas
+    get '/Pizzas' do
+      @pizzas = Pizza.all
+      erb :'Pizzas/index'
+    end
+    #
+
+
+    # show
+    get '/Pizzas/:id' do
+      @pizza = Pizza.find(params[:id])
+      erb(:"Pizzas/show")
+    end
+    #new
+    get '/Pizzas/new' do
+      @pizza = Pizza.new
+      erb :"Pizzas/new"
+    end    # edit
+
+    post '/Pizzas' do
+      @pizza = Pizza.new(params[:pizza])
+      if @pizza.save
+        redirect("/Pizzas/#{@pizza.id}")
+      else
+        erb :"Pizzas/new"
+      end
+    end
+
+    get '/Pizzas/:id/edit' do
+      @pizza = Pizza.find(params[:id])
+      erb :"Pizzas/edit"
+    end
+
+    # # update
+    # put '/artists/:id' do
+    #   @artist = Artist.find(params[:id])
+    #   if @artist.update_attributes(params[:artist])
+    #     redirect("/artists/#{artist.id}")
+    #   else
+    #     erb(:"artists/edit")
+    #   end
+    # end
+
+    # delete
+    delete '/pizzas/delete' do
+      @pizza = Pizza.find(params[:id])
+      if @pizza.destroy
+        redirect('/Pizzas')
+      else
+        redirect("/Pizzas/#{@pizza.id}")
+      end
+    end
 
   end
-end
--- VienVan/hw-w04-d04/pizza_shop/config.ru
+++ VienVan/hw-w04-d04/pizza_shop/config.ru
@@ -2,7 +2,10 @@ require 'rubygems'
 require 'bundler'
 Bundler.require
 
+#model
 require './models/pizza'
+
+#controller
 require './app'
 
 run PizzaShop
