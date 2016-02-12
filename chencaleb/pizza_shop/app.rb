53c71e85ccf90f75e063c74a7d800ede3296b0d6
chencaleb <clb_chen@berkeley.edu>
2016-02-11 21:55:38 -0800

    create

--- chencaleb/pizza_shop/app.rb
+++ chencaleb/pizza_shop/app.rb
@@ -7,4 +7,11 @@ class PizzaShop < Sinatra::Base
         erb :index
     end
 
+    #create
+    post "/pizzas" do
+    	params = {name: "Anchovy Explosion", sauce: 'red', cheese:true, mushrooms:false, extra_toppings: "anchovies"}
+    	@pizza = Pizza.new(params)
+    	@pizza.save
+  	end
+
 end
