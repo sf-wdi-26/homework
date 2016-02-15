181319979d8f2e9b22886814c2ff40e7edd48df4
nreddy216 <nreddy216@gmail.com>
2016-02-14 22:08:45 -0800

    begin todo app

--- nreddy216/w04d04/m3-methods-and-finders/starter-code/pizza_shop/app.rb
+++ nreddy216/w04d04/m3-methods-and-finders/starter-code/pizza_shop/app.rb
@@ -8,7 +8,7 @@ class PizzaShop < Sinatra::Base
     erb :"about"
   end
 
-  #get all pizzas
+  #get all pizzas ??? FIND OUT WHY
   get '/pizzas' do
     @pizzas = Pizza.all
     erb :"pizzas/index"
@@ -20,18 +20,27 @@ class PizzaShop < Sinatra::Base
     erb :"pizzas/new"
   end
 
-  #
-  # #create
+
+  #create
   post "/pizzas" do
-    # params = {name: "Red Anchovy Delight", sauce: 'red', cheese:true, mushrooms:true, extra_toppings: "anchoves"}
-    # @pizza = Pizza.create(params)
-    @pizzas = Pizza.all
-    erb :"pizzas/index"
+    @pizza = Pizza.new(params[:pizza])
+
+    if @pizza.save
+      redirect("/pizzas/#{@pizza.id}")
+    else
+      erb(:"pizzas/new")
+    end
   end
+
 
 
   #show individual pizza
   get "/pizzas/:id" do
+    @pizza = Pizza.find(params[:id])
+    erb :"pizzas/show"
+  end
+
+  post "/pizzas/:id" do
     @pizza = Pizza.find(params[:id])
     erb :"pizzas/show"
   end
@@ -45,7 +54,7 @@ class PizzaShop < Sinatra::Base
   #update
   put '/pizzas/:id' do
     @pizza = Pizza.find(params[:id])
-    if @pizza.update_attributes(params[:pizza])
+    if @pizza.update_attributes(params[:id])
       redirect("/pizzas/#{@pizza.id}")
     else
       erb :"pizzas/edit"
