6278035805d2518bf40a1dd24f148df9d16dbaa8
Beta-23 <alfred.dominguez@gmail.com>
2016-02-15 22:17:32 -0800

    Adds views files

--- Beta-23/w04-d05/wines-app/app.rb
+++ Beta-23/w04-d05/wines-app/app.rb
@@ -3,4 +3,22 @@ class WinesApp < Sinatra::Base
     "HAPPY VALENTINE DAY!"  
   end
 
+get "/wines" do
+  @wines = Wine.all
+  erb :"index"
+  end
+get "/wines/:id" do
+     @wines = Wines.all  
+     @wines = Wines.find(params[:id])
+     @wine.names
+  end 
+  get "/wines/new" do
+      erb :new
+  end
+post "/wines" do
+  @wine = Wine.create(params[:wine])
+  @wine.save
+  redirect "/"
+  end
+
 end
