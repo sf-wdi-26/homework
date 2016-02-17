db6c228dcd5d0a81a2a1547aff4aa5eca20eb082
Beta-23 <alfred.dominguez@gmail.com>
2016-02-16 21:07:28 -0800

    Eddits and adds migration files

--- Beta-23/practice_associations_lab/practice_associations/db/migrate/20160217042143_create_items.rb
+++ Beta-23/practice_associations_lab/practice_associations/db/migrate/20160217042143_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.references :order, index: true, foreign_key: true
-
+      
       t.timestamps null: false
     end
   end
--- Beta-23/practice_associations_lab/practice_associations/db/schema.rb
+++ Beta-23/practice_associations_lab/practice_associations/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217045115) do
+ActiveRecord::Schema.define(version: 20160217050457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,11 @@ ActiveRecord::Schema.define(version: 20160217045115) do
 
   create_table "items", force: :cascade do |t|
     t.integer  "order_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "name"
+    t.string   "description"
+    t.integer  "price"
   end
 
   add_index "items", ["order_id"], name: "index_items_on_order_id", using: :btree
