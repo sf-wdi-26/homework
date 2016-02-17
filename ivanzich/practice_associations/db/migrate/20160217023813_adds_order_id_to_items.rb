d605bf2ce0017da6e169e557322c4997aa13ad63
ivanzich <ivanzich@gmail.com>
2016-02-17 11:37:07 -0800

    W05D01

---
+++ ivanzich/practice_associations/db/migrate/20160217023813_adds_order_id_to_items.rb
@@ -0,0 +1,4 @@
+class AddsOrderIdToItems < ActiveRecord::Migration
+  def change
+  end
+end
---
+++ ivanzich/practice_associations/db/schema.rb
@@ -0,0 +1,28 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160217023813) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "price"
+    t.integer  "order_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
