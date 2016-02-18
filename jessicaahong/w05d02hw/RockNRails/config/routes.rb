ac2df49cd0fd88af7b75ca350490979b4c2cf468
jessicaahong <jessiehong9@gmail.com>
2016-02-17 16:14:06 -0800

    adds w05d02hw

---
+++ jessicaahong/w05d02hw/RockNRails/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get "/records", to: "records#index"
+
+  get "/records/new", to: "records#new"
+
+  post "/records", to: "records#create"
+
+  get "/records/:id", to: "records#show", as: "record"
+
+  delete "/records/:id", to: "records#destroy"
+end
