7995098a653f9e22fbf9bdce06f192ea6fc8d969
JDe-Lora <jdelora17@gmail.com>
2016-02-18 18:51:54 -0800

    finsihed step 18

--- JDe-Lora/bog_app/app/controllers/creatures_controller.rb
+++ JDe-Lora/bog_app/app/controllers/creatures_controller.rb
@@ -13,11 +13,23 @@ class CreaturesController < ApplicationController
 		creature = Creature.new(creature_params)
 		if creature.save
 			redirect_to creatures_path
-		end
+	end
 		def show
 			id = params[:id]
 			@creature = Creature.find(id)
 			render :show
 		end
+	def edit 
+		id = params[:id]
+		@creature = Creature.find(id)
+		render :edit
+	end
+	def update
+		creature_id = params[:id]
+		creature = Creature.find(creature_id)
+		update_attributes = params.require(:creature).permit(:name, :description)
+		creature.update_attributes(update_attributes)
+		redirect_to creature
+	end
 	end
 end
