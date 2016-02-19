1698cf38a05a2a5640bf107c2c079c8a549fc7ec
JDe-Lora <jdelora17@gmail.com>
2016-02-18 18:31:13 -0800

    at step 14

--- JDe-Lora/bog_app/app/controllers/creatures_controller.rb
+++ JDe-Lora/bog_app/app/controllers/creatures_controller.rb
@@ -3,4 +3,16 @@ class CreaturesController < ApplicationController
 		@creatures = Creature.all
 		render :index
 	end
+	def new
+		@creature = Creature.new
+		render :new
+	end
+
+	def create 
+		creature_params = params.require(:creature).permit(:name, :description)
+		creature = Creature.new(creature_params)
+		if creature.save
+			redirect_to creatures_path
+		end
+	end
 end
--- JDe-Lora/bog_app/config/routes.rb
+++ JDe-Lora/bog_app/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
     # We'll also use the resources method to have Rails 
     # make an index route for our creatures resource.
-    resources :creatures, only: [:index]
+    resources :creatures, only: [:index, :new, :create]
     # resources :creatures with :index is equivalent to adding:
     # get "/creatures", to: "creatures#index"
 
