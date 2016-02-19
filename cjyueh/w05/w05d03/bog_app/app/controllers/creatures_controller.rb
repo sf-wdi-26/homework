863f4a7331578a307f0029f24484166a7a5edd05
cjyueh <cjyueh@gmail.com>
2016-02-18 17:50:48 -0800

    creates create and show routes, controllers, and views

--- cjyueh/w05/w05d03/bog_app/app/controllers/creatures_controller.rb
+++ cjyueh/w05/w05d03/bog_app/app/controllers/creatures_controller.rb
@@ -6,4 +6,34 @@ class CreaturesController < ApplicationController
 		#render index view file (will have access to instance variables)
 		render :index
 	end
+
+	#show new creature form
+	def new
+		#new instance of Creature to view
+		@creature = Creature.new
+		render :new
+	end
+
+	#create new creature in db
+	def create
+		#validate params and save as variable
+		creature_params = params.require(:creature).permit(:name, :description)
+		#create new creature w above params
+		creature = Creature.new(creature_params)
+		#check that it saved
+		if creature.save
+			#if saved, redirect to route that shows all creatures
+			redirect_to creature #same as "/creatures/#{creature.id}"
+		end
+	end
+
+	#show a single creature
+	def show
+		#get id param from url
+		id = params[:id]
+		#find creature with that id and save to instance variable of Creature
+		@creature = Creature.find(id)
+		#render the show page with access to @creature variable
+		render :show
+	end
 end
--- cjyueh/w05/w05d03/bog_app/config/routes.rb
+++ cjyueh/w05/w05d03/bog_app/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'creatures#index'
 
-  resources :creatures, only: [:index]
+  resources :creatures, only: [:index, :new, :create, :show]
+  #resources :creatures with :new is equivalent to adding:
+  #get "/creatures/new", to: "creatures#new", as: "new_creature"
+  #post "/creatures", to: "creatures#create"
+  #get "/creatures/:id", to: "creatures#show", as: "creature"
 end
