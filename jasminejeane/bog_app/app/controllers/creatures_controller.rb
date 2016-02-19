d8f1b5f0c6cdd4c91dd6d93ad6bdba2bb3e7a1b5
Jasmine Martin <jasminejeane@gmail.com>
2016-02-18 22:47:16 -0800

    version 1 hw w05 d03

--- jasminejeane/bog_app/app/controllers/creatures_controller.rb
+++ jasminejeane/bog_app/app/controllers/creatures_controller.rb
@@ -9,6 +9,23 @@ def index
 
 def new
         render :new   # optional; this is the default behavior
+           @creature = Creature.new
+           render :new
+
+    end
+
+      # create a new creature in the database
+    def create
+        # validate params and save them as a variable
+        creature_params = params.require(:creature).permit(:name, :description)
+        # create a new creature with those params
+        creature = Creature.new(creature_params)
+        # check that it saved
+        if creature.save
+            # if saved, redirect to route that shows all creatures
+            redirect_to creatures_path
+            # ^ same as redirect_to "/creatures"
+        end
     end
 
 end
--- jasminejeane/bog_app/config/routes.rb
+++ jasminejeane/bog_app/config/routes.rb
@@ -2,10 +2,11 @@
 
 Rails.application.routes.draw do
     root to: 'creatures#index'
+    get '/new', to: 'creatures#new'
+    post '/creatures', to: 'creatures#create'
 
     # We'll also use the resources method to have Rails 
     # make an index route for our creatures resource.
-    resources :creatures, only: [:index, :new]
     # resources :creatures with :index is equivalent to adding:
     # get "/creatures", to: "creatures#index"
 end
