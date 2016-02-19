8f7f81ac82f274dae93b1053fa802857c5609b42
cjyueh <cjyueh@gmail.com>
2016-02-18 18:19:16 -0800

    creates update and delete routes, controllers, and views

--- cjyueh/w05/w05d03/bog_app/app/controllers/creatures_controller.rb
+++ cjyueh/w05/w05d03/bog_app/app/controllers/creatures_controller.rb
@@ -36,4 +36,42 @@ class CreaturesController < ApplicationController
 		#render the show page with access to @creature variable
 		render :show
 	end
+
+	#show edit form for specific creature
+	def edit
+		#save id param to variable
+		id = params[:id]
+		#find creature with that id and save to instance variable of Creature
+		@creature = Creature.find(id)
+		#render the edit form view with access to @creature variable
+		render :edit
+	end
+
+	#update specific creature in db
+	def update
+		#get id param from url
+		creature_id = params[:id]
+		#find creature to update by id
+		creature = Creature.find(creature_id)
+
+		#get updated creature data from params
+		updated_attributes = params.require(:creature).permit(:name, :description)
+		#update the creature
+		creature.update_attributes(updated_attributes)
+
+		#redirect to single creature show page for this creature
+		redirect_to creature #same as "/creatures/#{creature.id}"
+	end
+
+	#delete specific creature by id
+	def destroy
+		#save id param to variable
+		id = params[:id]
+		#find creature to delete by id
+		creature = Creature.find(id)
+		#destroy the creature
+		creature.destroy
+		#redirect to creatures index
+		redirect_to creatures_path #same as redirect_to "/creatures"
+	end
 end
