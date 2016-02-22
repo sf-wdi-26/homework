5a6d4bc2be047ddb8c8c3d87c0a476052121a401
nreddy216 <nreddy216@gmail.com>
2016-02-22 04:26:04 -0800

    library app homework fixing bug

---
+++ nreddy216/w05d05/lib_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def user_params
+    # params.require(:user)
+    params.require(:email)
+    params.require(:password)
+    params.permit(:email, :password)
+  end
+
+  def create
+    # user_params = params.require(:user).permit(:email, :password)
+    @user = User.confirm(user_params)
+    if @user
+      login(@user)
+      redirect_to "/users/#{@user.id}"
+    else
+      redirect_to "/sign_in"
+    end
+  end
+
+end
