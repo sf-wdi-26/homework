377488cdde3e2fe33ca7ecfcd71b7d1a14db38bc
Beta-23 <alfred.dominguez@gmail.com>
2016-02-21 21:27:13 -0800

    adds users index and new route

--- Beta-23/w05-d05/lib_app/config/routes.rb
+++ Beta-23/w05-d05/lib_app/config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
     get "/users", to: "users#index", as: "users"
 
+    get "/users/new", to: "users#new", as: "new_user"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
