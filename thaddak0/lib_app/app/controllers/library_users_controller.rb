48b7a02233bbe263363f2bdc72763056eca0c861
thaddak0 <annabellethaddeus@gmail.com>
2016-02-22 08:56:50 -0800

    homework commit

---
+++ thaddak0/lib_app/app/controllers/library_users_controller.rb
@@ -0,0 +1,20 @@
+class LibraryUsersController < ApplicationController
+
+  before_action :logged_in?
+
+  def index
+    @user = User.find(params[:user_id])
+    @libraries = @user.libraries
+
+    render :index
+  end
+
+  def create
+    @user = current_user
+    @library = Library.find(params[:library_id])
+    @user.libraries.push(@library)
+
+    redirect_to "/libraries/#{@library.id}"
+  end
+
+end
