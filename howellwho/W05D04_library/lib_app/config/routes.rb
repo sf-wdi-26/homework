9d1a63de76a9787117fa1f9e130ab217a0e7dabe
howellwho <howellwho@gmail.com>
2016-02-22 10:42:00 -0800

    homework

---
+++ howellwho/W05D04_library/lib_app/config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  root to: "users#index"
+
+  get "/users", to: "users#index", as: "users"
+  get "/users/new", to: "users#new", as: "new_user"
+  post "users", to: "users#create"
+  get "users/:id", to: "users#show"
+
+  get "/sign_in", to: "sessions#new"
+  post "/sessions", to: "sessions#create"
+
+  get "/libraries", to: "libraries#index"
+  get "/libraries/new", to: "libraries#new", as: "new_library"
+  post "/libraries", to: "libraries#create"
+  get "libraries/:id", to: "libraries#show"
+  get "libraries/edit/:id", to: "libraries#edit", as: "edit_library"
+  patch "libraries/edit/:id", to: "libraries#update"
+
+  get "users/:user_id/libraries", to: "library_users#index", as: "user_libraries"
+  post "libraries/:library_id/users", to: "library_users#create", as: "library_users"
+end
