5b7b20e3f3d103e1133d16ca706ff7e52c2cf49e
danfaeh <danfaeh@gmail.com>
2016-02-21 22:43:29 -0800

    library hw complete

---
+++ danfaeh/publicLibraryLab/lib_app/app/controllers/libraries_controller.rb
@@ -0,0 +1,24 @@
+class LibrariesController < ApplicationController
+
+  def index
+    @libraries = Library.all
+    current_user # sets @current_user
+    render :index
+  end
+
+  def new
+    @library = Library.new
+    render :new
+  end
+
+  def create
+    @library = Library.create(library_params)
+    redirect_to "/libraries"
+  end
+
+  private
+
+  def library_params
+    params.require(:library).permit(:name, :floor_count, :floor_area)
+  end
+end
