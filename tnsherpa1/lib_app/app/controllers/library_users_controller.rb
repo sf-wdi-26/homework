d83bc22b4812293ed686a94ba24e7aa9e846d61f
tnsherpa1 <tnsherpa1@gmail.com>
2016-02-22 08:42:37 -0800

    w05d04 lib_app

---
+++ tnsherpa1/lib_app/app/controllers/library_users_controller.rb
@@ -0,0 +1,17 @@
+class LibraryUsersController < ApplicationController
+	def index
+	@user = User.find(params[:user_id])
+	@libraries = @user.libraries
+
+	render :index
+	end
+
+	def create
+	@user = current_user
+    @library = Library.find(params[:library_id])
+    @user.libraries.push(@library)
+
+    redirect_to user_libraries(@user)
+	end
+
+end
