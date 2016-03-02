35de4b75c339d4553824283a01226823b3955420
jessicaahong <jessiehong9@gmail.com>
2016-03-02 07:59:13 -0800

    finishes stretches in w07d02hw

--- jessicaahong/w07d02hw/bite-me-mongo-lab/models/food.js
+++ jessicaahong/w07d02hw/bite-me-mongo-lab/models/food.js
@@ -1,9 +1,33 @@
 var mongoose = require('mongoose');
 
+
 var FoodSchema = new mongoose.Schema({
 	name: String,
 	yumminess: String
 });
 
+var RestaurantSchema = new mongoose.Schema({
+	name: {
+		type: String,
+	    default: ""
+	},
+	foods: [FoodSchema],
+	ingredients: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Ingredient'
+	}]
+});
+
+var IngredientSchema = new mongoose.Schema ({
+	name: {
+		type: String,
+		default: ""
+	}
+});
+
+var Restaurant = mongoose.model('Restaurant', RestaurantSchema);
 var Food = mongoose.model('Food', FoodSchema);
+var Ingredient = mongoose.model('Ingredient', IngredientSchema);
 module.exports = Food;
+module.exports = Restaurant;
+module.exports = Ingredient;
