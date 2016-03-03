84d29bb2da75355ba228f4b341990067e5d4f33b
Beta-23 <alfred.dominguez@gmail.com>
2016-03-03 00:02:42 -0800

    add's hw to repo

--- Beta-23/w07-d03/app/app.js
+++ Beta-23/w07-d03/app/app.js
@@ -13,6 +13,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Set up our app to accept to use EJS
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname + '/public'));
 
 // our routes
