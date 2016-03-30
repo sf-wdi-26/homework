34f91cc9ab5968c1681b2ad93d68484c55b006e4
takshingli810 <takshingli810@gmail.com>
2016-03-29 18:36:19 -0700

    reconstruction of folders

---
+++ takshingli810/w02/d04/base.js
@@ -0,0 +1,42 @@
+// globals
+var weeklyQuakesEndpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
+var $infoRowTarget;
+var map;
+
+function fetchQuakeData(){
+  $.get(weeklyQuakesEndpoint, function(response){
+    response.features.forEach(function(quake) {
+      var title = quake.properties.title;
+      var hoursAgo = Math.round(
+        (Date.now() - quake.properties.time ) / (1000*60*60)
+      );
+      
+      $infoRowTarget.append("<p>")
+                    .append(title + " / " + hoursAgo + " hours ago");
+      
+      var lat = quake.geometry.coordinates[1];
+      var lng = quake.geometry.coordinates[0];
+      
+      new google.maps.Marker({
+        position: new google.maps.LatLng(lat,lng),
+        map: map,
+        title: title
+      });
+    });
+  });
+}
+
+// render a map
+function createMap(){
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 37.78, lng: -122.44},
+    zoom: 1
+  });
+}
+
+function init() {
+  $infoRowTarget = $("#info");
+  //render a map
+  createMap();
+  fetchQuakeData();
+}
